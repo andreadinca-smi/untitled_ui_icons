fc69e75cd3d8b10cbbae7aa1f3ba171c9460b918
Andrea D'Incà <andrea.dinca@sanmarcoinformatica.it>
2026-04-13 16:33:11 +0200

    feat: add home icon metadata

--- lib/src/icons.dart
+++ lib/src/icons.dart
@@ -94,6 +94,7 @@ class UntitledIcons {
   static const String headphones01 = 'headphones-01';
   static const String heartHand = 'heart-hand';
   static const String heart = 'heart';
+  static const String home = 'home';
   static const String homeLine = 'home-line';
   static const String homeSmile = 'home-smile';
   static const String icon = 'icon';
